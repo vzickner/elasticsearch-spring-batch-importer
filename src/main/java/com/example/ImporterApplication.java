2756dda54d12d5d3f3dd25b1d133d38d1183b1e8
Valentin Zickner <valentin.zickner@mimacom.com>
2016-10-08 12:37:43 +0200

    Initial commit (downloaded with elasticsearch, jdbc, mysql, batch)

---
+++ src/main/java/com/example/ImporterApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImporterApplication.class, args);
+	}
+}
